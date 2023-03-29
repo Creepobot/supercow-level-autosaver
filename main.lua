87b00e35db1ec460bd589da6fd1f05dfd282dd29
Creepobot Neo <61410619+Creepobot@users.noreply.github.com>
2023-03-30 02:31:32 +0300

    Add config support

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local imgui = require("imgui")
+local config = require("config")
 local memory = require("memory")
 local timers = require("timers")
 local ffi = require("ffi")
@@ -8,6 +9,11 @@ local editorScene = memory.at("68 ? ? ? ? 8B 4D ? E8 ? ? ? ? 8B 4D ? 8B 55"):add
 local isPaused = memory.at("C6 05 ? ? ? ? ? 0F B6 0D ? ? ? ? 85 C9 75 ? 6A"):add(2):readOffset()
 local gameSaveFunc = memory.at("55 8B EC D9 05 ? ? ? ? D8 1D ? ? ? ? DF E0 F6 C4 ? 75 ? EB ? E8"):getFunction("void (*)()")
 local saveFunc = function() if baseScene:readInt() == editorScene then gameSaveFunc() end end
+
+local buf
+if config.mod:has("saveInterval") then
+    buf = ffi.new("int[1]", config.mod:getNumber("saveInterval"))
+else buf = ffi.new("int[1]", 60) end
 local timerId = timers.setInterval(saveFunc, 60000)
 
 function HelpMarker(text)
@@ -31,8 +37,11 @@ function render()
     end
 end
 
-local buf = ffi.new("int[1]", 60)
 function renderUi()
+    if imgui.Button("GitHub") then
+        os.execute('start "" "https://github.com/Creepobot/supercow-level-autosaver"')
+    end
+    
     imgui.Text("Частота автосохранения")
     HelpMarker("Частота в секундах, с которой игра\n автоматически сохраняет уровень.")
     imgui.SliderInt(" ", buf, 15, 900, "%0i сек")
@@ -41,5 +50,7 @@ function renderUi()
     if imgui.Button("Применить") then
         timers.clearTimer(timerId)
         timerId = timers.setInterval(saveFunc, buf[0] * 1000)
+        config.mod:set("saveInterval", buf[0])
+        config.save()
     end
 end
